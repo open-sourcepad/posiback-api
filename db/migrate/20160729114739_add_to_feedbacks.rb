ca15c81c56ef43cbf5914731da547f201ff3aa54
Alvin Garcia <alving@sourcepad.com>
2016-07-29 20:11:24 +0800

    fix db migration

--- db/migrate/20160729114739_add_to_feedbacks.rb
+++ db/migrate/20160729114739_add_to_feedbacks.rb
@@ -1,4 +1,6 @@
 class AddToFeedbacks < ActiveRecord::Migration[5.0]
   def change
+    add_column :feedbacks, :category, :string
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729105612) do
+ActiveRecord::Schema.define(version: 20160729114739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160729105612) do
     t.text    "content"
     t.integer "nansiback_id"
     t.integer "nasiback_id"
+    t.string  "category"
   end
 
   create_table "users", force: :cascade do |t|
