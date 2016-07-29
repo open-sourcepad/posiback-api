9bbe2f95b8e8513c76c79a5fc9a3c4a7e6b48e2b
Migz Suelto <migz.suelto@gmail.com>
2016-07-29 22:17:11 +0800

    user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: 'michaels@sourcepad.com', password: '12341234')
-User.create(email: 'admin@posiback.com', password: 'password')
+
+# Create Users
+users = YAML::load_file(Rails.root.join('db', 'data', 'users.yml'))
+users.each do |user|
+  current_user = User.where(email: user["email"]).first
+  unless current_user.present?
+    new_user = User.new(user)
+    new_user.save!
+  end
+end
